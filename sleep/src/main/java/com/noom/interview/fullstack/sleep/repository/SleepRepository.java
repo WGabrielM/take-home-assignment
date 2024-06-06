5d41b33fcff3945d608fdec50d9fde5b32cc69af
Wellington <wellingtongmb@gmail.com>
2024-06-06 07:58:15 -0300

    repository improvement

--- sleep/src/main/java/com/noom/interview/fullstack/sleep/repository/SleepRepository.java
+++ sleep/src/main/java/com/noom/interview/fullstack/sleep/repository/SleepRepository.java
@@ -3,6 +3,7 @@ package com.noom.interview.fullstack.sleep.repository;
 import com.noom.interview.fullstack.sleep.entity.SleepEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -11,7 +12,7 @@ import java.util.List;
 @Repository
 public interface SleepRepository extends JpaRepository<SleepEntity, Long> {
 
-    @Query("SELECT s FROM SleepEntity s WHERE s.date BETWEEN :init AND :end")
-    public List<SleepEntity> takeThePeriod(LocalDate init, LocalDate end);
+    @Query("SELECT s FROM SleepEntity s WHERE s.date BETWEEN :startDate AND :endDate")
+    List<SleepEntity> findSleepEntitiesWithinPeriod(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 
 }
