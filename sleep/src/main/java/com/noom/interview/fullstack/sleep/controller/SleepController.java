8d62f25b0fe569142a2f381f8cb5ac7b0b449b10
Wellington <wellingtongmb@gmail.com>
2024-06-06 07:54:39 -0300

    controller improvement

--- sleep/src/main/java/com/noom/interview/fullstack/sleep/controller/SleepController.java
+++ sleep/src/main/java/com/noom/interview/fullstack/sleep/controller/SleepController.java
@@ -3,6 +3,7 @@ package com.noom.interview.fullstack.sleep.controller;
 import com.noom.interview.fullstack.sleep.dto.SleepAverageDTO;
 import com.noom.interview.fullstack.sleep.dto.SleepResponseDTO;
 import com.noom.interview.fullstack.sleep.entity.SleepEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.noom.interview.fullstack.sleep.services.SleepService;
 import org.springframework.http.ResponseEntity;
@@ -12,32 +13,51 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/sleep")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class SleepController {
 
     @Autowired
     private SleepService sleepService;
 
+
+    /**
+     * Creates a new sleep entry.
+     *
+     * @param sleepEntity the sleep entity to create
+     * @return the created sleep entity
+     */
+
     @PostMapping
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public ResponseEntity<Object> createSleep(@RequestBody SleepEntity sleepEntity) {
         try {
-            var result = this.sleepService.saveSleep(sleepEntity);
+            var result = sleepService.saveSleep(sleepEntity);
             return ResponseEntity.ok().body(result);
-        }catch (Exception e){
-            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body("Internal Server Error");
         }
     }
 
+    /**
+     * Retrieves all sleep entries.
+     *
+     * @return the list of all sleep entries
+     */
     @GetMapping
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public ResponseEntity<List<SleepResponseDTO>> getAll() {
         var result = sleepService.listAll();
         return ResponseEntity.ok(result);
     }
 
+    /**
+     * Retrieves the average sleep statistics.
+     *
+     * @return the average sleep statistics
+     */
     @GetMapping("/average")
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public ResponseEntity<SleepAverageDTO> getSleepAverage() {
         var result = sleepService.getSleepAvarage();
         return ResponseEntity.ok(result);
